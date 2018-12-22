5241902e9972b6a495ee1b0fbcd8335965a4dfa6
Parth Mehrotra <parth@mehrotra>
2018-12-22 12:56:18 -0500

    spec

--- src/payserver.js
+++ src/payserver.js
@@ -1,56 +1,25 @@
-import { WalletClient } from 'bclient';
-import { Network } from 'bcoin';
-import WebSocket from 'ws';
-import redis from 'redis';
+class RecieptServer {
+    /**
+     *  For each winner:
+     *      Generate a list of "Peers"
+     *      Check each wallet for confirmed balance
+     *      Update status as it becomes available
+     *      When all peer's balances have 1 confirmation submit the winning transaction
+     *      Send transactionID to UI
+     *      Offer a download to summary.txt
+     */
 
-import Client from './client';
-
-class Payserver {
-    constructor() {
-        this.redisClientPlayers = this.connectToRedis(6379, 'redis-players')
-        this.redisClientGames = this.connectToRedis(6379, 'redis-gameservers')
-        this.server = new WebSocket.Server({ port: 7000 });
-        this.clients = {};
-        this.server.on('connection', this.newCustomer.bind(this));
-    }
-
-    connectToRedis(port, name) {
-        let client = redis.createClient(port, name)
-        client.on('connect', function() {
-            console.log("connected " + name)
-        });
-        client.on('error', function(err) {
-            console.log('Something went wrong ', err)
-        });
-        return client
-    }
-
-    newCustomer(connection) {
-        let token = this.generateNewToken(8);
-        this.clients[token] = new Client(connection, token, this.getRate(), this.redisClientPlayers, this.redisClientGames);
-    }
-
-    generateNewToken(n) {
-        var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        for (var i = 0; i < n; i++)
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
-        return text;
-    }
-
-    getRate() {
-        return 15000;
-    }
+     /**
+      *  Frontend experience
+      *     Congratulations on winning, your token was [TOKEN] <click to reveal>. Treat this as a password, if something goes 
+      *     wrong reach out to support@sneks.io and provide them with your token. If you lose this, you'll need to prove you
+      *     own your private keys by authoring a transaction that we describe. 
+      *
+      *     Please enter where you want us to send the winnings to. If you'd like us to send the winnings
+      *     to your original wallet press "Original wallet". (Do not use if you used an exchange)
+      *
+      *     Transaction status:
+      *     Transaction posted: TX-ID
+      *     Click to download receipt.txt
+      */ 
 }
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-(async () => {
-    try {
-        new Payserver();
-    } catch (err) {
-        console.log(err)
-    }
-})()
